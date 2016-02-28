adae1d3c16f511b8558b9c6b83acceef4e1d0161
Sebastian Sobociński <hiddenboox@gmail.com>
2016-02-28 17:01:19 +0100

    added webpack build system, compass, ts support

---
+++ src/app/app.tsx
@@ -0,0 +1,9 @@
+import './styles/main.scss';
+
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+
+ReactDOM.render(
+    <h1>Hello, world!</h1>,
+    document.body
+);
---
+++ webpack.config.js
@@ -0,0 +1,3 @@
+/**
+ * Created by hidde on 28.02.2016.
+ */
